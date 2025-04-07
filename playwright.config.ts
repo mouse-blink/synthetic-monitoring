24b573e5c00e17b325bf92ffa3809406b306f9d8
mouse <narek.kostanyan@inspace.chat>
2025-04-07 21:58:09 +0200

    feat: add background remover monitor

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,9 +20,9 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? "blob" : "html",
+  reporter: [["line"], [process.env.CI ? "blob" : "html"]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -32,6 +32,7 @@ export default defineConfig({
     trace: "on-first-retry",
   },
 
+  timeout: 240_000,
   /* Configure projects for major browsers */
   projects: [
     {
@@ -44,12 +45,11 @@ export default defineConfig({
       use: { ...devices["Desktop Firefox"] },
     },
 
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
-
     /* Test against mobile viewports. */
+    // {
+    //   name: "webkit",
+    //   use: { ...devices["Desktop Safari"] },
+    // },
     // {
     //   name: 'Mobile Chrome',
     //   use: { ...devices['Pixel 5'] },
